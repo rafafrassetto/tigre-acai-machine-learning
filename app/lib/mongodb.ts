6e5c22c012d106a34f5b29f112073c39bdaa8204
Rafael Frassetto Pereira <88680818+rafafrassetto@users.noreply.github.com>
2026-04-10 20:18:37 -0300

    feat : conexao mongodb

--- app/lib/mongodb.ts
+++ app/lib/mongodb.ts
@@ -1,6 +1,6 @@
 import { MongoClient } from "mongodb"
 
-const uri = process.env.MONGODB_URI as string
+const uri = process.env.MONGODB_URI || "mongodb://localhost:27017"
 const options = {}
 
 let client
